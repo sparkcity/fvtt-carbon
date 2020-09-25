cfa18bb8df321233606f5fd3e9c33c7f7b66d5e2
Parker <23155686+sparkcity@users.noreply.github.com>
2020-09-25 13:08:41 -0400

    v1.0
    Character sheet complete.
    
    * Currency counts against weight when it shouldn't. Will look into this.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fvtt-carbon
- 
+# Carbon
+A module to provide character sheet support for playing in a cyberpunk setting in a 5e world.
--- src/carbon.js
+++ src/carbon.js
@@ -1,6 +1,6 @@
-import Actor5e from "/systems/dnd5e/module/actor/entity.js";
-import { DND5E } from '/systems/dnd5e/module/config.js';
-import CompactBeyond5eSheet from "../../../modules/compact-beyond-5e-sheet/foundryvtt-compactBeyond5eSheet.js"
+import Actor5e from "../../../systems/dnd5e/module/actor/entity.js";
+import { DND5E } from "../../../systems/dnd5e/module/config.js"
+import { CompactBeyond5eSheet } from "../../../modules/compact-beyond-5e-sheet/foundryvtt-compactBeyond5eSheet.js"
 
 //changing abilities to system abilities
 DND5E.abilities["wis"] = "Technology";
@@ -27,7 +27,10 @@ DND5E.skills["smo"] = "Sense Motive";
 const prep = Actor5e.prototype.prepareBaseData;	
 function extendActorData() {
 	if(this.data.type === "npc" || this.data.type === "character") {
-		const skl = this.data.data.skills;
+    const skl = this.data.data.skills;
+    const atr = this.data.data.attributes;
+    const curr = this.data.data.currency;
+    //skills
     skl["bur"] = skl["bur"] || {value: 0, ability: "cha"};
 		skl["gmb"] = skl["gmb"] || {value: 0, ability: "int"};
 		skl["nvg"] = skl["nvg"] || {value: 0, ability: "int"};
@@ -37,12 +40,18 @@ function extendActorData() {
     skl["trk"] = skl["trk"] || {value: 0, ability: "int"};
     skl["var"] = skl["var"] || {value: 0, ability: "wis"};
     skl["vla"] = skl["vla"] || {value: 0, ability: "dex"};
+    //attributes
+    atr["bloodTox"] = atr["bloodTox"] || 0;
+    atr["corpInf"] = atr["corpInf"] || 0;
+    atr["streetInf"] = atr["streetInf"] || 0; 
+    //currency
+    curr["₩"] = curr["₩"] || 0;
 	}
 	return prep.call(this);
 }
 Actor5e.prototype.prepareBaseData = extendActorData;
 
-//Registering default sheet
+//Registering as default sheet
   class CarbonCompactSheet extends CompactBeyond5eSheet {
     static get defaultOptions() {
         const options = super.defaultOptions;
@@ -55,30 +64,33 @@ Actor5e.prototype.prepareBaseData = extendActorData;
     makeDefault: true
   });
 
-//FOR EDITING DEFAULT CHARACTER SHEET
+//Adding elements to CarbonCompact sheet
 Hooks.on("renderActorSheet", (app, html, data) => {
-	const counters = html.find("div.counters");
-	const flags = data.actor.flags.carbon || {};
-	counters.append(`
-	<div class="counter flexrow corpInf">
-    <h4>Corporate Influence</h4>
-    <div class="counter-value">
-      <input type="text" name="flags.carbon.corpInf" placeholder="0" value="${flags.corpInf ?? 0}" data-dtype="Number"/>
-    </div>
+	const traits = html.find("div.traits");
+  traits.prepend(`
+  <div class="form-group {{#unless data.attributes.bloodTox}}inactive{{/unless}}">
+    <label>B. Tox.</label>
+    <input type="text" name="data.attributes.bloodTox" data-dtype="Number" placeholder="0"
+      value="${data.data.attributes.bloodTox}" />
   </div>
-  
-	<div class="counter flexrow streetInf">
-    <h4>Street Influence</h4>
-    <div class="counter-value">
-      <input type="text" name="flags.carbon.streetInf" placeholder="0" value="${flags.streetInf ?? 0}" data-dtype="Number"/>
-    </div>
+  <div class="form-group {{#unless data.attributes.corpInf}}inactive{{/unless}}">
+    <label>Cp. Inf.</label>
+    <input type="text" name="data.attributes.corpInf" data-dtype="Number" placeholder="0"
+      value="${data.data.attributes.corpInf}" />
   </div>
-  
-	<div class="counter flexrow bloodTox">
-    <h4>Blood Toxicity</h4>
-    <div class="counter-value">
-      <input type="text" name="flags.carbon.bloodTox" placeholder="0" value="${flags.bloodTox ?? 0}" data-dtype="Number"/>
-    </div>
-	</div>
+  <div class="form-group {{#unless data.attributes.streetInf}}inactive{{/unless}}">
+    <label>St. Inf.</label>
+    <input type="text" name="data.attributes.streetInf" data-dtype="Number" placeholder="0"
+      value="${data.data.attributes.streetInf}" />
+  </div>
   `);
+
+/* @todo: only show W currency, none of the others
+  const curr = html.find("div.currency-inputs");
+  curr.remove("label.denomination.gp");
+  curr.remove("label.denomination.pp");
+  curr.remove("label.denomination.cp");
+  curr.remove("label.denomination.ep");
+  curr.remove("label.denomination.sp"); 
+  */
 });
